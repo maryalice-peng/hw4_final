4256a535650e9d9d7dd005d82cedeeb8f81aa505
MaryAlice Peng <maryalice.peng@gmail.com>
2025-03-19 07:44:53 +0000

    Fixed user_id in places table

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,48 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    if @current_user
+      @places = Place.where({ "user_id" => @current_user["id"] })
+    else
+      @places = []  # Leave it empty
+    end
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+
+    if @place && @current_user && @place["user_id"] == @current_user["id"]
+      @entries = Entry.where({ "place_id" => @place["id"] }).order("created_at DESC")
+    else
+      if @current_user
+        flash["notice"] = "Oops, you're not authorized to view this place."
+      else
+        flash["notice"] = "Please login first."
+      end
+      redirect_to "/places"
+    end
   end
 
   def new
+    if @current_user
+      @place = Place.new
+    else
+      flash["notice"] = "Please login first."
+      redirect_to "/login"
   end
+end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    if @current_user
+      @place = Place.new
+      @place["name"] = params["name"]
+      @place["user_id"] = @current_user["id"]  # Link to the user that is using Tacosgram
+      @place.save
+      redirect_to "/places"
+    else
+      flash["notice"] = "Please login first."
+      redirect_to "/login"
+    end
   end
 
-end
+end 
